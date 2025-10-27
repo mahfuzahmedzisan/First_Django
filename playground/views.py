d7b20ef5c711e6ed68532a7492c0f1b1ce020e07
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-10-27 17:14:32 +0600

    render a html file with new endpoint

--- playground/views.py
+++ playground/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
     return HttpResponse("Hello, welcome to the Playground app!")
+def hello_template(request):
+    return render(request, 'hello.html')
